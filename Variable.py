24ea867862acf881d60aa11dcd07b9e502b760d5
Felicialite19 <felicialite@gmail.com>
2024-09-01 22:35:46 +0400

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,5 @@
+number_of_task=12
+number_of_hours_spent=1.5
+curse_name='Python'
+time_for_one_task=number_of_hours_spent/number_of_task
+print('Курс: ',curse_name,', всего задач: ',number_of_task,', затрачено часов: ',number_of_hours_spent,', среднее время выполнения ',time_for_one_task,' часа',sep='')
