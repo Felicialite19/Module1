331e8e8024ea77019807c88951899e5916bd63ed
Felicialite19 <felicialite@gmail.com>
2024-09-05 21:48:49 +0400

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,16 @@
+my_dict={'Ann':25,'Don':31,'Alex':40,'Rose':19}
+print('Dict: ',my_dict)
+print('Existing value: ',my_dict['Ann'])
+my_dict['Lisa']=22
+my_dict.update({'Sam':52, 'Kate':28})
+Del_value=my_dict['Don']
+del my_dict['Don']
+print('Not existing value: ',my_dict.get('Don'))
+print('Deleted value: ',Del_value)
+print('Modified dictionary: ',my_dict)
+
+my_set={1,2,5,'Text',5,2}
+print('Set: ',my_set)
+my_set.add((6,7))
+my_set.discard(2)
+print('Modified set: ',my_set)
